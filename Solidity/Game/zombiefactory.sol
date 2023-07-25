eff59dce7e5e42229ba285d3eb7d6aabdfaccdce
Pavel Syrovatskii <pavel1107syr@gmail.com>
2023-07-25 16:13:34 +0300

    protect with onlyOwn, use modifier, added gas optimization, added loops

--- Solidity/Game/zombiefactory.sol
+++ Solidity/Game/zombiefactory.sol
@@ -1,6 +1,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -10,6 +12,8 @@ contract ZombieFactory {
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint23 readytime;
     }
 
     Zombie[] public zombies;
@@ -32,6 +36,7 @@ contract ZombieFactory {
     function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
